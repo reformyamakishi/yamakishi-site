bcafb2471ebf777e46ac4e6df53df67c18b54464
reformyamakishi <reformyamakishi@gmail.com>
2026-08-03 02:31:59 +0000

    icon-48x48.png への link を追加（ブックマーク・ショートカット用）

--- wp/header.php
+++ wp/header.php
@@ -17,6 +17,7 @@ $asset = get_stylesheet_directory_uri();
 <link rel="icon" href="<?php echo esc_url( home_url( '/favicon.ico' ) ); ?>" sizes="any">
 <link rel="icon" type="image/png" sizes="32x32" href="<?php echo esc_url( $asset ); ?>/assets/img/favicon/icon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="<?php echo esc_url( $asset ); ?>/assets/img/favicon/icon-16x16.png">
+<link rel="icon" type="image/png" sizes="48x48" href="<?php echo esc_url( $asset ); ?>/assets/img/favicon/icon-48x48.png">
 <link rel="apple-touch-icon" sizes="180x180" href="<?php echo esc_url( $asset ); ?>/assets/img/favicon/apple-touch-icon-180x180.png">
 <link rel="manifest" href="<?php echo esc_url( $asset ); ?>/assets/img/favicon/manifest.json">
 <meta name="msapplication-TileColor" content="#fe3301">
